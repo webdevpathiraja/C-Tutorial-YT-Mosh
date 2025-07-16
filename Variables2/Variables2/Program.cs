d74e206e93fdcffb2cee529f7d70abf28aaa8116
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-07-16 22:31:39 +0530

    constants

--- Variables2/Variables2/Program.cs
+++ Variables2/Variables2/Program.cs
@@ -7,20 +7,26 @@ namespace Variables2
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 19.99f;
-            char letter = 'A';
-            bool isWorking = true;
-            string firstName = "John"; 
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 19.99f;
+            //char letter = 'A';
+            //bool isWorking = true;
+            //string firstName = "John"; 
 
 
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(letter);
-            Console.WriteLine(isWorking);
-            Console.WriteLine(firstName);
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(letter);
+            //Console.WriteLine(isWorking);
+            //Console.WriteLine(firstName);
+
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+
         }
     }
 }
