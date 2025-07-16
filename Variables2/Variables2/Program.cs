ffc4228f24332bbd4b8aa34c44829d63480b4c31
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-07-16 22:22:14 +0530

    Add Variables2 project demonstrating basic variable types in C#

---
+++ Variables2/Variables2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Variables2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 19.99f;
+            char letter = 'A';
+            bool isWorking = true;
+            string firstName = "John"; 
+
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(letter);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(firstName);
+        }
+    }
+}
