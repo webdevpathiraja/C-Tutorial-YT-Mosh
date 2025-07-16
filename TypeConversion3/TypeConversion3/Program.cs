b913f230c4fd7ac9ce5aa4f553c4049319562efb
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-07-16 22:55:14 +0530

    added TypeConversion3 with exception handling

---
+++ TypeConversion3/TypeConversion3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+
+namespace TypeConversion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            int i = 1;
+            byte b = (byte)i; // Explicit conversion from int to byte
+            Console.WriteLine(b);
+
+            int i2 = 1000;
+            byte b2 = (byte)i2; // Explicit conversion from int to byte, may lose data
+            Console.WriteLine(b2);
+
+
+            // Convert.ToInt32()
+            string number = "123"; // same => var number = "123";
+            //int i = int(number); // string to int not allowed
+            int i3 = Convert.ToInt32(number); // Explicit conversion from string to int
+            Console.WriteLine(i3);
+
+
+
+            // overflow exception => byte cant handle 1000
+            //var largeInt = "1234";
+            //byte b3 = Convert.ToByte(largeInt);
+            //Console.WriteLine(b3);
+
+
+            try
+            {
+                var largeInt = "1234";
+                byte b3 = Convert.ToByte(largeInt);
+                Console.WriteLine(b3);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("number could not be converted to byte");
+            }
+         
+
+
+
+        }
+    }
+}
